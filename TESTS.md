9c2f5075bb032f3c58dc6d64e7280c9560f18f21
DaveMarchuk <110414567+DaveMarchuk@users.noreply.github.com>
2022-09-24 11:16:10 -0700

    Tested prototype to calculate the cost through branching. It will check the toppings and size then return a final price for pizza.

--- TESTS.md
+++ TESTS.md
@@ -20,3 +20,20 @@ Expected Output: ["Tomatoes","Pepperoni","Cheese"]
 
 Test: "My previous test was only able to pass through strings but not arrays. With the updated code, you should now be able to pass in an array. Later I will pass in an array of checkbox values that will get passed in and stored in the toppings."
 Code: 
+Pizza.prototype.addTopping = function(toppings) {
+  let pizzaArray = [];
+
+  for (let i = 0; i < this.pizzaToppings.length - 1; i++) {
+    pizzaArray.push(toppings[i]);
+  }
+  this.pizzaToppings = pizzaArray;
+  return this.pizzaToppings;
+};
+Expected Output: ["Tomatoes","cheese","meat","duck egg"]
+
+Describe: Pizza.prototype.calculateCost(toppings,size)
+
+Test: "I test to see if the prototype is able to successfully iterate through the array and able to add to the price. Then return the new price."
+Code: Pizza.prototype.calculateCost = function(size) {...}
+  let costTracker = 0.00;
+Expected Output: 3.00
--- js/scripts.js
+++ js/scripts.js
@@ -12,14 +12,63 @@ function isEmpty()  {
 function Pizza(toppings,size) {
   this.pizzaToppings = toppings;
   this.size = size;
+  this.price = 0.00;
 }
 
 Pizza.prototype.addTopping = function(toppings) {
   let pizzaArray = [];
 
   for (let i = 0; i < this.pizzaToppings.length - 1; i++) {
-    pizzaArray.push(toppings[i]);
+    pizzaArray.push(toppings[i].toUpperCase());
   }
   this.pizzaToppings = pizzaArray;
   return this.pizzaToppings;
 };
+
+Pizza.prototype.calculateCost = function(size) {
+  let costTracker = 0.00;
+
+  if (size === "small") {
+    costTracker = 4.00;
+  }
+
+  if (size === "medium")  {
+    costTracker = 6.00;
+  }
+
+  if (size === "large") {
+    costTracker = 8.00;
+  }
+
+  for (let i = 0; i < this.pizzaToppings.length; i++) {
+    if (this.pizzaToppings[i] === "PEPPERONI")  {
+      costTracker += 1.00;
+    } 
+    
+    if  (this.pizzaToppings[i] === "PEPPERJACK")  {
+      costTracker += 1.00;
+    }
+
+    if  (this.pizzaToppings[i] === "OLIVES")  {
+      costTracker += 0.50;
+    }
+
+    if  (this.pizzaToppings[i] === "TOMATOES")  {
+      costTracker += 0.50;
+    }
+
+    if (this.pizzaToppings[i] === "BACON")  {
+      costTracker += 1.00;
+    }
+
+    if (this.pizzaToppings[i] === "CHICKEN")  {
+      costTracker += 1.00;
+    }
+
+    if (this.pizzaToppings[i] === "MOZARELLA")  {
+      costTracker += 1.00;
+    }
+  }
+  this.price = costTracker;
+  return this.price;
+}
